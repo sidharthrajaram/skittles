9a5224dfb6a9e7e0fd3e9de3349d05f418ea5ee0
Sidharth Rajaram <sidharthrajaram@gmail.com>
2017-09-03 01:11:01 -0700

    m8

--- algo.py
+++ algo.py
@@ -1,13 +1,38 @@
-#arithmetic classifier
-import numpy as np
-from sklearn import preprocessing
+#runtime
 
-np.random.seed(1)
+def convert(input):
 
-#activation fns
-def sigmoid(X):
-	return 1/(1+np.exp(-X))
+	arithmetic = ''
 
-def sigmoid_deriv(output):
-	return output*(1-output)
+	words = input.split()
+	print(words)
+	
+	for i in range(len(words)):
+		if(words[i].lower() == 'add'): 
+			words[i] = '+'
+		elif(words[i].lower() == 'gets'):
+			words[i] = '+'
+
+	# converted = " ".join(x for x in words)
+	for j in range(len(words)):
+		if(words[j] == '+'):
+			arithmetic += words[j]
+		else:
+			try: 
+				num = int(words[j])
+				arithmetic += str(num)
+
+			except(ValueError):
+				pass
+
+	return arithmetic
+
+problem = input("Give me a problem: ")
+print(problem)
+print()
+
+arithmetic = convert(problem)
+print()
+print("In arithmetic:")
+print(arithmetic)
 
--- run.py
+++ run.py
@@ -1,12 +1,71 @@
 #runtime
+addwords = ['gets','get','more','add','adds','got','added']
+subwords = ['gave','lost','away']
+add_count = 0
+sub_count = 0
+operation = ''
+
+def addition(problem):
+	count = 0
+	for word in problem:
+		for key in addwords:
+			if(word.lower() == key):
+				count += 1
+	return count
+
+
+def subtraction(problem):
+	count = 0
+	for word in problem:
+		for key in subwords:
+			if(word.lower() == key):
+				count += 1
+	return count
+
+
+#find operation between set of numbers?
+
+def convert(input):
+
+	arithmetic = ''
+
+	words = input.split()
+	print(words)
+
+	add_count = addition(words)
+	print(add_count)
+	sub_count = subtraction(words)
+	print(sub_count)
+
+	if(add_count > sub_count):
+		operation = '+'
+	else:
+		operation = '-'
+
+	numbers = []
+	for word in words:
+		try: 
+			num = int(word)
+			numbers.append(str(num))
+		except(ValueError):
+			pass
+			
+	print(numbers)
+	for a in range(len(numbers)):
+		if(a == len(numbers)-1):
+			arithmetic += numbers[a]
+		else:
+			arithmetic += numbers[a]
+			arithmetic += operation
+
+	return arithmetic
 
 problem = input("Give me a problem: ")
 print(problem)
 print()
 
-arithmetic = ''
-for w in problem.split():
-	if(w.lower() == 'add'):
-		print("+")
-	else:
-		print(w)
+arithmetic = convert(problem)
+print()
+print("In arithmetic:")
+print(arithmetic)
+
