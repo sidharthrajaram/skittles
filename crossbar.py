0d7d4e3b34c494762639435aecf55de0ba85013d
Sidharth Rajaram <sidharthrajaram@gmail.com>
2017-09-09 21:13:03 -0700

    set up multiply stuff

--- crossbar.py
+++ crossbar.py
@@ -1,10 +1,6 @@
-#main word problem solver
+#multiplication division testbed
 
-addwords = ['gets','get','more','add','adds','got','added','received','gives']
-subwords = ['gave','lost','away','takes','take', 'took','taking','losing','loses']
-
-add_count = 0
-sub_count = 0
+multwords = ["each"]
 operation = ''
 operators = []
 numbers = []
@@ -20,77 +16,6 @@ def findNumbers():
 	print("numbers in problem (in order):")
 	print(numbers)
 
-def findOperators():
-	init_index = 0
-	end_index = 0
-
-	for i in range(len(problem)-1):
-
-		for num in numbers:
-
-			if(problem[i] == num):
-				#print("***********************")
-				init_index = i
-				# print("inIT")
-				# print(init_index)
-
-				start = init_index+1
-
-				while(start < len(problem)):
-
-					for num in numbers:
-
-						if(problem[start] == num):
-							end_index = start
-							break
-
-					if(end_index == start):
-						break
-
-					start+=1
-
-				first = init_index
-				last = end_index
-				# print("indices")
-				# print(first)
-				# print(last)
-				# print()
-
-				if(first == last):
-					break
-
-				#the count up
-				addnum = 0
-				subnum = 0
-
-				while(first < last):
-					for key in addwords:
-						if(problem[first].lower() == key):
-							#print("ADD WORD FOUND")
-							addnum+=1
-
-					for key in subwords:
-						if(problem[first].lower() == key):
-							#print("SUB WORD FOUND")
-							subnum+=1
-
-					first+=1
-
-				# print("add")
-				# print(addnum)
-				# print("sub")
-				# print(subnum)
-
-				if(addnum > subnum):
-					operators.append('+')
-				else:
-					operators.append('-')
-			pass
-
-	print()
-	print("problem operators (in order):")
-	print(operators)
-
 problem_input = input("Give me a problem: ")
 print()
 
@@ -98,7 +23,8 @@ problem = problem_input.split()
 
 findNumbers()
 print()
-findOperators()
+# findAddSumOperators()
+
 
 
 
--- plusminus.py
+++ plusminus.py
@@ -1,5 +1,6 @@
-#main word problem solver
+#addition subtraction 
 
+#keywords (nltk soon)
 addwords = ['gets','get','more','add','adds','got','added','received','gives','receives']
 subwords = ['gave','lost','away','takes','take', 'took','taking','losing','loses']
 
@@ -19,67 +20,39 @@ def findNumbers():
 	print("numbers in problem (in order):")
 	print(numbers)
 
+#finds operators in problem statement (+ or -)
 def findAddSumOperators():
 	init_index = 0
 	end_index = 0
 
 	for i in range(len(problem)-1):
-
 		for num in numbers:
-
 			if(problem[i] == num):
-				#print("***********************")
 				init_index = i
-				# print("inIT")
-				# print(init_index)
-
 				start = init_index+1
 
 				while(start < len(problem)):
-
 					for num in numbers:
-
 						if(problem[start] == num):
 							end_index = start
 							break
-
 					if(end_index == start):
 						break
-
 					start+=1
-
 				first = init_index
 				last = end_index
-				# print("indices")
-				# print(first)
-				# print(last)
-				# print()
-
 				if(first == last):
 					break
-
-				#the count up
 				addnum = 0
 				subnum = 0
-
 				while(first < last):
 					for key in addwords:
 						if(problem[first].lower() == key):
-							#print("ADD WORD FOUND")
 							addnum+=1
-
 					for key in subwords:
 						if(problem[first].lower() == key):
-							#print("SUB WORD FOUND")
 							subnum+=1
-
 					first+=1
-
-				# print("add")
-				# print(addnum)
-				# print("sub")
-				# print(subnum)
-
 				if(addnum > subnum):
 					operators.append('+')
 				else:
@@ -90,6 +63,7 @@ def findAddSumOperators():
 	print("problem operators (in order):")
 	print(operators)
 
+#test
 problem_input = input("Give me a problem: ")
 print()
 
